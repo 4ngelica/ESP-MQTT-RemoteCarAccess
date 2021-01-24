6f210a7f38e3467636d22b1573adc42acd42cbcc
Angélica Batassim <47900225+4ngelica@users.noreply.github.com>
2021-01-24 14:15:46 -0300

    Add files via upload

---
+++ sketch-blue-joycon.ino
@@ -0,0 +1,416 @@
+//Programa: NodeMCU e MQTT - Remote Car Control IoT
+
+#include <Ultrasonic.h> //Importa a Biblioteca Ultrasonic
+#include <ESP8266WiFi.h> // Importa a Biblioteca ESP8266WiFi
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClient
+
+ 
+
+//Definindo a id mqtt e tópicos para publicação e subscribe
+#define TOPICO_SUBSCRIBE "mqttInterfaceEnvia"     //tópico MQTT de recepção de mensagem
+#define TOPICO_PUBLISH   "mqttNodeMCURecebe"      //tópico MQTT de envio de mensagem
+                                                   
+#define ID_MQTT  "IdCarroMestre"     //id mqtt (para identificação de sessão)                                
+ 
+//Definindo mapeamento de pinos do NodeMCU
+#define D0    16
+#define D1    5
+#define D2    4
+#define D3    0
+#define D4    2
+#define D5    14
+#define D6    12
+#define D7    13
+#define D8    15
+#define D9    3
+#define D10   1
+ 
+// WIFI
+const char* SSID = "suaRedeWifi"; // SSID / nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = "suaSenha"; // Senha da rede WI-FI que deseja se conectar
+  
+// MQTT
+const char* BROKER_MQTT = "broker.hivemq.com"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883; // Porta do Broker MQTT
+ 
+ 
+//Variáveis e objetos globais
+WiFiClient espClient; // Cria o objeto espClient
+PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
+char EstadoSaida = '0';  //variável que armazena o estado atual da saída
+
+// Define os pinos de controle da L298N
+int leftMotorForward = 2;     /* GPIO2(D4) -> IN3   */
+int rightMotorForward = 15;   /* GPIO15(D8) -> IN1  */
+int leftMotorBackward = 0;    /* GPIO0(D3) -> IN4   */
+int rightMotorBackward = 13;  /* GPIO13(D7) -> IN2  */
+
+
+// Define os pinos de habilitar a L298N
+int rightMotorENB = 14; /* GPIO14(D5) -> Motor-A Enable */
+int leftMotorENB = 12;  /* GPIO12(D6) -> Motor-B Enable */
+  
+//Prototypes
+void initSerial();
+void initWiFi();
+void initMQTT();
+void reconectWiFi(); 
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void VerificaConexoesWiFIEMQTT(void);
+void InitOutput(void);
+int teste = 1;
+
+
+//Define os pinos do sensor ultrassônico e implementa a função de leitura
+Ultrasonic ultrasonic(5, 4);
+int distance;
+
+
+/* 
+ *  Implementações das funções
+ */
+void setup() 
+{
+    //inicializações:
+    InitOutput();
+    initSerial();
+    initWiFi();
+    initMQTT();
+    Serial.begin(9600);
+
+  //Inicializa os pinos de controle do motor como outputs
+  pinMode(leftMotorForward, OUTPUT);
+  pinMode(rightMotorForward, OUTPUT); 
+  pinMode(leftMotorBackward, OUTPUT);  
+  pinMode(rightMotorBackward, OUTPUT);
+
+  //Inicializa os pinos de habilitado do motor como outputs
+  pinMode(leftMotorENB, OUTPUT); 
+  pinMode(rightMotorENB, OUTPUT);
+}
+  
+//Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial 
+//        o que está acontecendo.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initSerial() 
+{
+    Serial.begin(115200);
+}
+ 
+//Função: inicializa e conecta-se na rede WI-FI desejada
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initWiFi() 
+{
+    delay(10);
+    Serial.println("------Conexao WI-FI------");
+    Serial.print("Conectando-se na rede: ");
+    Serial.println(SSID);
+    Serial.println("Aguarde");
+     
+    reconectWiFi();
+}
+  
+//Função: inicializa parâmetros de conexão MQTT(endereço do 
+//        broker, porta e seta função de callback)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initMQTT() 
+{
+    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+    MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+}
+  
+//Função: função de callback 
+//        esta função é chamada toda vez que uma informação de 
+//        um dos tópicos subescritos chega)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void mqtt_callback(char* topic, byte* payload, unsigned int length) 
+{
+    String msg;
+ 
+    //Obtém a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+   
+    //toma ação dependendo da string recebida:
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+    
+    if (msg.equals("L"))
+    {
+       digitalWrite(D0, LOW);
+        EstadoSaida = '1';
+        MotorForward();
+    }
+ 
+    //verifica se deve colocar nivel alto de tensão na saída D0:    
+    if (msg.equals("D"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '0';
+        MotorStop();
+    }
+
+    // gira para esquerda
+    if (msg.equals("A"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        TurnLeft();
+        delay(200);
+        MotorStop();
+        
+    }
+
+     //gira para direita
+    if (msg.equals("B"))
+    {
+         digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        TurnRight();
+        delay(200);
+        MotorStop();
+    }
+
+    //recuar
+    if (msg.equals("C"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        MotorBackward();
+    }
+   
+
+    /*
+    //joycon vermelho avança
+    if (msg.equals("E"))
+    {
+        digitalWrite(D0, LOW);
+        EstadoSaida = '1';
+        MotorForward();
+    }
+    
+    //joycon vermelho para
+    if (msg.equals("F"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '0';
+        MotorStop();
+    }
+
+    // joycon vermelho gira para esquerda
+    if (msg.equals("G"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        TurnLeft();
+        delay(200);
+        MotorStop();
+    }
+
+    //joycon vermelho gira para direita
+    if (msg.equals("H"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        TurnRight();
+        delay(200);
+        MotorStop();
+    }
+
+    //joycon vermelho recua
+    if (msg.equals("I"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        MotorBackward();
+    }*/
+     
+}
+  
+//Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
+//        em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconnectMQTT() 
+{
+    while (!MQTT.connected()) 
+    {
+        Serial.print("* Tentando se conectar ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT)) 
+        {
+            Serial.println("Conectado com sucesso ao broker MQTT!");
+            MQTT.subscribe(TOPICO_SUBSCRIBE); 
+        } 
+        else
+        {
+            Serial.println("Falha ao reconectar no broker.");
+            Serial.println("Havera nova tentativa de conexao em 2s");
+            delay(2000);
+        }
+    }
+}
+  
+//Função: reconecta-se ao WiFi
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconectWiFi() 
+{
+    //se já está conectado a rede WI-FI, nada é feito. 
+    //Caso contrário, são efetuadas tentativas de conexão
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+         
+    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+     
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(100);
+        Serial.print(".");
+    }
+   
+    Serial.println();
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(SSID);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+}
+ 
+//Função: verifica o estado das conexões WiFI e ao broker MQTT. 
+//        Em caso de desconexão (qualquer uma das duas), a conexão
+//        é refeita.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void VerificaConexoesWiFIEMQTT(void)
+{
+    if (!MQTT.connected()) 
+        reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+     
+     reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+ 
+//Função: envia ao Broker o estado atual do output 
+//Parâmetros: nenhum
+//Retorno: nenhum
+void EnviaEstadoOutputMQTT(void)
+{
+    if (EstadoSaida == '0')
+      MQTT.publish(TOPICO_PUBLISH, "D");
+ 
+    if (EstadoSaida == '1')
+      MQTT.publish(TOPICO_PUBLISH, "L");
+ 
+    Serial.println("- Estado da saida D0 enviado ao broker!");
+    delay(1000);
+}
+ 
+//Função: inicializa o output em nível lógico baixo
+//Parâmetros: nenhum
+//Retorno: nenhum
+void InitOutput(void)
+{
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+    pinMode(D0, OUTPUT);
+    digitalWrite(D0, HIGH);          
+}
+
+void TurnRight(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+  teste = 1;
+  
+}
+
+/********************************************* BACKWARD *****************************************************/
+void TurnLeft(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorBackward,HIGH);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+  teste = 1;
+  
+}
+
+/********************************************* TURN LEFT *****************************************************/
+void MotorForward(void)    
+{
+  teste = 2;
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH); 
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(rightMotorForward,HIGH);
+  digitalWrite(rightMotorBackward,LOW);
+  digitalWrite(leftMotorBackward,HIGH);  
+}
+
+/********************************************* TURN RIGHT *****************************************************/
+void MotorBackward(void)   
+{
+  digitalWrite(leftMotorENB,HIGH);
+  digitalWrite(rightMotorENB,HIGH);
+  digitalWrite(leftMotorForward,HIGH);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,HIGH);
+  digitalWrite(leftMotorBackward,LOW);
+  teste = 1;
+}
+
+/********************************************* STOP *****************************************************/
+void MotorStop(void)   
+{
+  digitalWrite(leftMotorENB,LOW);
+  digitalWrite(rightMotorENB,LOW);
+  digitalWrite(leftMotorForward,LOW);
+  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(rightMotorBackward,LOW);
+  teste = 1;
+}
+ 
+//programa principal
+void loop() 
+{   
+
+   //Parar de acordo com a leitura do sensor
+    if(ultrasonic.read() <= 40 && teste==2){
+            MotorStop();
+      MQTT.publish(TOPICO_PUBLISH, "Z");
+    }
+   // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+
+  Serial.print("Distância em CM: ");
+  Serial.println(distance);
+  delay(200);
+  if(distance <= 40){
+    Serial.print("Obstáculo a menos de 40 cm! Os carros vão parar.");
+    delay(5000);
+  }
+ 
+    
+    //garante funcionamento das conexões WiFi e ao broker MQTT
+    VerificaConexoesWiFIEMQTT();
+ 
+    //envia o status de todos os outputs para o Broker no protocolo esperado
+    EnviaEstadoOutputMQTT();
+ 
+    //keep-alive da comunicação com broker MQTT
+    MQTT.loop();
+}
